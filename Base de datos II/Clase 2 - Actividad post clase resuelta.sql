bc21bfb9ae9067171fee5bfc4988e199583518f1
LucioFex <luciano1d2esteban@gmail.com>
2025-08-09 00:22:34 -0300

    vault backup: 2025-08-09 00:22:34

--- Base de datos II/Clase 2 - Actividad post clase resuelta.sql
+++ Base de datos II/Clase 2 - Actividad post clase resuelta.sql
@@ -1,8 +1,3 @@
-#clase_2 
-
-![[Clase 2 - Actividad post clase resuelta.sql]]
-
-```sql
 USE lesteban25
 
 CREATE TABLE dbo.t_tipo_producto (
@@ -26,12 +21,12 @@ CREATE TABLE dbo.t_producto (
 );
 
 
--- Creaciï¿½n de ï¿½ndices:
+-- Creación de índices:
 
--- Relaciï¿½n entre producto y su tipo
+-- Relación entre producto y su tipo
 CREATE NONCLUSTERED INDEX IX_producto_id_tipo_producto ON dbo.t_producto (id_tipo_producto);
 
--- Relaciï¿½n entre producto y su marca
+-- Relación entre producto y su marca
 CREATE NONCLUSTERED INDEX IX_producto_marca_id ON dbo.t_producto (marca_id);
 
 
@@ -44,19 +39,19 @@ DBCC CHECKIDENT ('dbo.t_tipo_producto', RESEED, 0);
 
 
 INSERT INTO dbo.t_tipo_producto (descripcion) VALUES
-('Electrï¿½nica'), 
+('Electrónica'), 
 ('Ropa'), 
-('Hogar y Jardï¿½n'), 
+('Hogar y Jardín'), 
 ('Alimentos y Bebidas'), 
 ('Juguetes'),
 ('Deportes'), 
-('Libros y Papelerï¿½a'), 
+('Libros y Papelería'), 
 ('Mascotas'), 
 ('Herramientas'), 
 ('Belleza y Cuidado Personal');
 
 INSERT INTO dbo.t_marca (descripcion) VALUES
-('TecnoMundo'), ('VisteBien'), ('CasaFï¿½cil'), ('Delicias S.A.'), ('GlobalCorp'),
+('TecnoMundo'), ('VisteBien'), ('CasaFácil'), ('Delicias S.A.'), ('GlobalCorp'),
 ('Sportiva'), ('Letras Vivas'), ('MundoAnimal'), ('Ferrex'), ('BellaPiel'),
 ('GamerZone'), ('AudioPro'), ('ChefMaestro'), ('EcoVida'), ('Infantiles'),
 ('Aventura Extrema'), ('OfiTech'), ('DulceHogar'), ('MotorPlus'), ('Saludable');
@@ -65,16 +60,16 @@ INSERT INTO dbo.t_producto (descripcion, precio, id_tipo_producto, marca_id) VAL
 -- Productos 1-10
 ('Smartphone X1', 899990.50, 1, 1), 
 ('Remera Estampada', 25500.00, 2, 2), 
-('Lï¿½mpara de Escritorio', 32000.75, 3, 3),
+('Lámpara de Escritorio', 32000.75, 3, 3),
 ('Galletitas de Chocolate', 1850.00, 4, 4), 
 ('Laptop Pro', 1850000.00, 1, 5), 
-('Jean Clï¿½sico', 48000.00, 2, 2),
-('Auto a Fricciï¿½n', 12500.99, 5, 15), 
-('Cafï¿½ Molido 500g', 8900.00, 4, 4), 
+('Jean Clásico', 48000.00, 2, 2),
+('Auto a Fricción', 12500.99, 5, 15), 
+('Café Molido 500g', 8900.00, 4, 4), 
 ('Teclado USB', 21800.00, 1, 3),
-('Almohada Viscoelï¿½stica', 35000.00, 3, 18),
+('Almohada Viscoelástica', 35000.00, 3, 18),
 -- Productos 11-20
-('Balï¿½n de Fï¿½tbol', 29900.00, 6, 6), 
+('Balón de Fútbol', 29900.00, 6, 6), 
 ('Set de Mancuernas 5kg', 45000.00, 6, 16), 
 ('Botella de Agua 1L', 15000.00, 6, 6),
 ('Yoga Mat', 25500.50, 6, 20), 
@@ -85,40 +80,40 @@ INSERT INTO dbo.t_producto (descripcion, precio, id_tipo_producto, marca_id) VAL
 ('Alimento para Perro 15kg', 85000.00, 8, 8),
 ('Rascador para Gato', 42000.00, 8, 8),
 -- Productos 21-30
-('Correa Paseo Retrï¿½ctil', 19500.00, 8, 18), 
+('Correa Paseo Retráctil', 19500.00, 8, 18), 
 ('Juguete Hueso Goma', 9800.00, 8, 8),
 ('Set de Destornilladores', 38000.00, 9, 9), 
 ('Taladro Percutor 500W', 95000.00, 9, 9), 
 ('Caja de Herramientas', 55000.00, 9, 19),
-('Cinta Mï¿½trica 5m', 12000.00, 9, 9), 
+('Cinta Métrica 5m', 12000.00, 9, 9), 
 ('Crema Hidratante Facial', 28000.00, 10, 10), 
 ('Protector Solar FPS 50', 25000.00, 10, 20),
 ('Set de Maquillaje', 49900.00, 10, 10), 
-('Shampoo Orgï¿½nico', 19900.00, 10, 20),
+('Shampoo Orgánico', 19900.00, 10, 20),
 -- Productos 31-40
 ('Monitor Gamer 24"', 450000.00, 1, 11),
-('Auriculares Inalï¿½mbricos', 95000.00, 1, 12), 
+('Auriculares Inalámbricos', 95000.00, 1, 12), 
 ('Buzo con Capucha', 65000.00, 2, 6), 
 ('Zapatillas Urbanas', 89000.00, 2, 16),
-('Juego de Sï¿½banas Queen', 58000.00, 3, 18), 
+('Juego de Sábanas Queen', 58000.00, 3, 18), 
 ('Batidora de Mano', 42000.00, 3, 13), 
 ('Snack de Frutos Secos', 6500.00, 4, 20),
 ('Jugo de Naranja 1.5L', 3200.00, 4, 4), 
-('Muï¿½eca Articulada', 29500.00, 5, 15), 
-('Mouse Ergonï¿½mico', 35000.00, 1, 17),
+('Muñeca Articulada', 29500.00, 5, 15), 
+('Mouse Ergonómico', 35000.00, 1, 17),
 -- Productos 41-50
-('Pantalï¿½n de Jogging', 49500.00, 2, 6), 
-('Taza de Cerï¿½mica', 9900.00, 3, 18), 
+('Pantalón de Jogging', 49500.00, 2, 6), 
+('Taza de Cerámica', 9900.00, 3, 18), 
 ('Aceite para Motor', 25000.00, 9, 19),
-('Vela Aromï¿½tica', 14000.00, 3, 10), 
+('Vela Aromática', 14000.00, 3, 10), 
 ('Libro de Cocina Vegana', 32000.00, 7, 20), 
 ('Pelota de Tenis x3', 11000.00, 6, 6),
 ('Comida para Gato 3kg', 25000.00, 8, 8), 
 ('Martillo', 18000.00, 9, 9), 
-('Mï¿½scara de Pestaï¿½as', 16500.00, 10, 10),
+('Máscara de Pestañas', 16500.00, 10, 10),
 ('Silla de Oficina', 180000.00, 3, 17);
 
--- Visualizaciï¿½n general de datos
+-- Visualización general de datos
 
 SELECT * FROM dbo.t_tipo_producto;
 SELECT * FROM dbo.t_marca;
@@ -144,11 +139,12 @@ ORDER BY
 /*
 producto				precio		tipo_de_producto	id_producto	 marca
 ----------------------------------------------------------------------------------
-Smartphone X1	        899990.50	Electrï¿½nica			1			 TecnoMundo
+Smartphone X1	        899990.50	Electrónica			1			 TecnoMundo
 Remera Estampada	    25500.00	Ropa				2			 VisteBien
-Lï¿½mpara de Escritorio	32000.75	Hogar y Jardï¿½n		3			 CasaFï¿½cil
+Lámpara de Escritorio	32000.75	Hogar y Jardín		3			 CasaFácil
 Galletitas de Chocolate	1850.00		Alimentos y Bebidas	4			 Delicias S.A.
-Laptop Pro	            1850000.00	Electrï¿½nica			5			 GlobalCorp
+Laptop Pro	            1850000.00	Electrónica			5			 GlobalCorp
 ...						...			...					...			 ...
 */
-```
+
+
